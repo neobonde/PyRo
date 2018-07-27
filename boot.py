31c37fe5761edd45e15fecd10dbb19c0284abe86
Nicolai Bonde <neobonde@gmail.com>
2018-07-28 01:29:54 +0200

    Added boot.py to connect to internet as station
    Note: SSID info redacted

---
+++ boot.py
@@ -0,0 +1,10 @@
+import network
+import webrepl
+sta_if = network.WLAN(network.STA_IF)
+if not sta_if.isconnected():
+    print('connecting to network...')
+    sta_if.active(True)
+    sta_if.connect('', '')
+    while not sta_if.isconnected():
+        pass
+print('network config:', sta_if.ifconfig())
