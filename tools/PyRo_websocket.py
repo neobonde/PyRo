0710d77f989ef4c231c8dc2994b4eb2518353b99
Nicolai Bonde <neobonde@gmail.com>
2018-07-28 15:04:16 +0200

    Added uploading tool

---
+++ tools/PyRo_websocket.py
@@ -0,0 +1,79 @@
+from __future__ import print_function
+import sys
+import os
+import struct
+try:
+    import usocket as socket
+except ImportError:
+    import socket
+import websocket_helper
+
+DEBUG = 0
+
+WEBREPL_REQ_S = "<2sBBQLH64s"
+WEBREPL_PUT_FILE = 1
+WEBREPL_GET_FILE = 2
+WEBREPL_GET_VER  = 3
+
+
+def debugmsg(msg):
+    if DEBUG:
+        print(msg)
+
+class websocket:
+
+        def __init__(self, s):
+            self.s = s
+            self.buf = b""
+
+        def write(self, data, text=False):
+            OpCode = 1 if text else 2
+            l = len(data)
+            if l < 126:
+                # TODO: hardcoded "binary" type
+                hdr = struct.pack(">BB", 0x80+OpCode, l)
+            else:
+                hdr = struct.pack(">BBH", 0x80+OpCode, 126, l)
+            self.s.send(hdr)
+            self.s.send(data)
+
+        def recvexactly(self, sz):
+            res = b""
+            while sz:
+                data = self.s.recv(sz)
+                if not data:
+                    break
+                res += data
+                sz -= len(data)
+            return res
+
+        def read(self, size, text_ok=False):
+            if not self.buf:
+                while True:
+                    hdr = self.recvexactly(2)
+                    assert len(hdr) == 2
+                    fl, sz = struct.unpack(">BB", hdr)
+                    if sz == 126:
+                        hdr = self.recvexactly(2)
+                        assert len(hdr) == 2
+                        (sz,) = struct.unpack(">H", hdr)
+                    if fl == 0x82:
+                        break
+                    if text_ok and fl == 0x81:
+                        break
+                    debugmsg("Got unexpected websocket record of type %x, skipping it" % fl)
+                    while sz:
+                        skip = self.s.recv(sz)
+                        debugmsg("Skip data: %s" % skip)
+                        sz -= len(skip)
+                data = self.recvexactly(sz)
+                assert len(data) == sz
+                self.buf = data
+
+            d = self.buf[:size]
+            self.buf = self.buf[size:]
+            assert len(d) == size, len(d)
+            return d
+
+        def ioctl(self, req, val):
+            assert req == 9 and val == 2
---
+++ tools/upload.py
@@ -0,0 +1,144 @@
+#This file is based on webrepl_cli.py
+
+
+import struct, sys, os, socket, time
+
+from PyRo_websocket import websocket, WEBREPL_REQ_S, WEBREPL_PUT_FILE, WEBREPL_GET_FILE, WEBREPL_GET_VER
+
+import websocket_helper
+
+
+DEBUG = 0
+SANDBOX= ""
+
+def debugmsg(msg):
+    if DEBUG:
+        print(msg)
+
+def read_resp(ws):
+    data = ws.read(4)
+    sig, code = struct.unpack("<2sH", data)
+    assert sig == b"WB"
+    return code
+
+def send_req(ws, op, sz=0, fname=b""):
+    rec = struct.pack(WEBREPL_REQ_S, b"WA", op, 0, 0, sz, len(fname), fname)
+    debugmsg("%r %d" % (rec, len(rec)))
+    ws.write(rec)
+
+def put_file(ws, local_file, remote_file):
+    sz = os.stat(local_file)[6]
+    dest_fname = (SANDBOX + remote_file).encode("utf-8")
+    rec = struct.pack(WEBREPL_REQ_S, b"WA", WEBREPL_PUT_FILE, 0, 0, sz, len(dest_fname), dest_fname)
+    debugmsg("%r %d" % (rec, len(rec)))
+    ws.write(rec[:10])
+    ws.write(rec[10:])
+    assert read_resp(ws) == 0
+    cnt = 0
+    with open(local_file, "rb") as f:
+        while True:
+            sys.stdout.write("Sent %d of %d bytes\r" % (cnt, sz))
+            sys.stdout.flush()
+            buf = f.read(1024)
+            if not buf:
+                break
+            ws.write(buf)
+            cnt += len(buf)
+    print()
+    assert read_resp(ws) == 0
+
+def get_ver(ws):
+    send_req(ws, WEBREPL_GET_VER)
+    d = ws.read(3)
+    d = struct.unpack("<BBB", d)
+    return d
+
+def login(ws, passwd):
+    while True:
+        c = ws.read(1, text_ok=True)
+        if c == b":":
+            assert ws.read(1, text_ok=True) == b" "
+            break
+    ws.write(passwd.encode("utf-8") + b"\r")
+
+
+def send_cmd(ws, cmd):
+    print("Doint REPL command: " + cmd)
+    bcmd = str.encode(cmd+'\r')
+    ws.write(bcmd, text=True)
+    time.sleep(1) # Just to slow down comm
+
+TARGET_IP = "192.168.87.107"
+TARGET_PORT = "8266"
+TARGET_PASS = "1234"
+
+def main():
+
+    print("Uploading src")
+    s = socket.socket()
+
+    ai = socket.getaddrinfo(TARGET_IP, TARGET_PORT)
+    addr = ai[0][4]
+
+    print (addr)
+    s.connect(addr)
+    #s = s.makefile("rwb")
+    websocket_helper.client_handshake(s)
+
+    ws = websocket(s)
+
+    login(ws, TARGET_PASS)
+    print("Remote WebREPL version:", get_ver(ws))
+
+    # Now commands can be sent and files can be uploaded
+
+    send_cmd(ws,"\x03") # Break currently running program if any
+    
+    send_cmd(ws,"import os")
+    send_cmd(ws,"import machine")
+
+    thisFileDir = os.path.dirname(os.path.abspath(__file__))
+    srcDir = os.path.join(thisFileDir,'..', 'src')
+
+    rootDir = os.listdir(srcDir)
+
+    # Go through source and look for folders
+    # For each folder in src
+        # Send create folder command
+        # Send go to folder command
+        # Put all files from folder in target folder
+        # Return to previous folder
+    # For the rest of the files just upload in root
+
+    for item in rootDir:
+        fullPath = os.path.join(srcDir,item)
+        print(fullPath)
+        if os.path.isdir(fullPath):
+            cmd = "os.mkdir('%s')"%item
+            send_cmd(ws,cmd)
+
+            cmd = "os.chdir('%s')"%item
+            send_cmd(ws,cmd)
+            
+            subDir = os.listdir(fullPath)
+            
+            for subItem in subDir:
+                subFullPath = os.path.join(fullPath,subItem)
+                print(subFullPath)
+                put_file(ws, subFullPath, subItem)
+            
+            cmd = "os.chdir('..')"
+            send_cmd(ws,cmd)
+            
+
+        else:
+            put_file(ws, fullPath, item)
+
+
+    cmd = "machine.reset()"
+    send_cmd(ws,cmd)
+
+    # Nothing can be done after this
+    s.close()
+
+main()
